182f9840b3283c477ff2300f497e0239ba3d688e
Nikolay Dorofeev <dorich2000@gmail.com>
2022-08-20 17:54:31 +0400

    Убрать авторизацию через Email
    - Блок авторизации через Email закомментирован
    - Страница регистрации через Email внесена в `.nuxtignore`

--- components/AuthPanel.vue
+++ components/AuthPanel.vue
@@ -3,42 +3,37 @@ import {useAuthStore} from "~/stores/auth"
 
 const authStore = useAuthStore()
 const supabase = useSupabaseClient()
-const loginInputs = ref({ email: '', password: '' })
-
-function signInWithEmail() {
-  console.log('email')
-}
+// const loginInputs = ref({ email: '', password: '' })
+//
+// function signInWithEmail() {
+//   console.log('email')
+// }
 
 function signInWithGoogle() {
   authStore.signInWithGoogle(supabase)
-}
-async function signout() {
-  await authStore.signOut(supabase)
 }
 
 </script>
 
 <template>
   <Dialog header="Авторизация" v-model:visible="visible" @update:visible="$emit('update:visible')">
-    <form @submit.prevent="signInWithEmail" >
-      <span class="p-input-icon-right">
-          <InputText type="email" v-model="loginInputs.email" />
-          <i class="pi pi-user" />
-      </span>
-      <span class="p-input-icon-right">
-          <InputText type="password" v-model="loginInputs.password" />
-          <i class="pi pi-lock" />
-      </span>
-      <nuxt-link to="/auth/register">
-        <Button class="p-button-text">Регистрация</Button>
-      </nuxt-link>
-      <Button type="submit">
-        Войти
-      </Button>
-    </form>
-    <template #footer>
-      <Button @click="signInWithGoogle">Google</Button>
-    </template>
+    <Button @click="signInWithGoogle">Google</Button>
+<!--    <form @submit.prevent="signInWithEmail" >-->
+<!--      <span class="p-input-icon-right">-->
+<!--          <InputText type="email" v-model="loginInputs.email" />-->
+<!--          <i class="pi pi-user" />-->
+<!--      </span>-->
+<!--      <span class="p-input-icon-right">-->
+<!--          <InputText type="password" v-model="loginInputs.password" />-->
+<!--          <i class="pi pi-lock" />-->
+<!--      </span>-->
+<!--      <nuxt-link to="/auth/register">-->
+<!--        <Button class="p-button-text">Регистрация</Button>-->
+<!--      </nuxt-link>-->
+<!--      <Button type="submit">-->
+<!--        Войти-->
+<!--      </Button>-->
+<!--    </form>-->
   </Dialog>
 
 </template>
@@ -52,18 +47,6 @@ export default {
   emits: ['update:visible'],
   data(){
     return {
-      rules: {
-        email: {
-          required: true,
-          message: 'Пожалуйста, введите email',
-          trigger: 'blur'
-        },
-        password: {
-          required: true,
-          message: 'Поожалуйста, введите пароль',
-          trigger: ['input', 'blur']
-        }
-      }
     }
   }
 
